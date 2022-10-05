ad8f4de19552d03a113c6dc73e3b89b7535bf8f8
Vong Xiong <vxiong@students.ntc.edu>
2022-10-05 14:10:18 -0500

    update connection strings

---
+++ SRC/NTC_Lego/NTC_Lego/Server/Migrations/20221005190620_updateNullable.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NTC_Lego.Server.Migrations
+{
+    public partial class updateNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inventory_Location_LocationId",
+                table: "Inventory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inventory_Product_ProductId",
+                table: "Inventory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchaseOrder_Supplier_SupplierId",
+                table: "PurchaseOrder");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchaseOrderDetail_PurchaseOrder_PurchaseOrderId",
+                table: "PurchaseOrderDetail");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WarehouseName",
+                table: "Warehouse",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SupplierName",
+                table: "Supplier",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SaleOrderTotalPrice",
+                table: "SaleOrder",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SaleOrderDate",
+                table: "SaleOrder",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PurchaseOrderSubTotalPrice",
+                table: "PurchaseOrderDetail",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PurchaseOrderId",
+                table: "PurchaseOrderDetail",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PurchaseOrderDetailQuantity",
+                table: "PurchaseOrderDetail",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SupplierId",
+                table: "PurchaseOrder",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PurchaseOrderTotalPrice",
+                table: "PurchaseOrder",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PurchaseOrderDate",
+                table: "PurchaseOrder",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ProductWeight",
+                table: "Product",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ProductPrice",
+                table: "Product",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductName",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductDescription",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductColor",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BinName",
+                table: "Location",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "Inventory",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "Inventory",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inventory_Location_LocationId",
+                table: "Inventory",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inventory_Product_ProductId",
+                table: "Inventory",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchaseOrder_Supplier_SupplierId",
+                table: "PurchaseOrder",
+                column: "SupplierId",
+                principalTable: "Supplier",
+                principalColumn: "SupplierId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchaseOrderDetail_PurchaseOrder_PurchaseOrderId",
+                table: "PurchaseOrderDetail",
+                column: "PurchaseOrderId",
+                principalTable: "PurchaseOrder",
+                principalColumn: "PurchaseOrderId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inventory_Location_LocationId",
+                table: "Inventory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inventory_Product_ProductId",
+                table: "Inventory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchaseOrder_Supplier_SupplierId",
+                table: "PurchaseOrder");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchaseOrderDetail_PurchaseOrder_PurchaseOrderId",
+                table: "PurchaseOrderDetail");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WarehouseName",
+                table: "Warehouse",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SupplierName",
+                table: "Supplier",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SaleOrderTotalPrice",
+                table: "SaleOrder",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SaleOrderDate",
+                table: "SaleOrder",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PurchaseOrderSubTotalPrice",
+                table: "PurchaseOrderDetail",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PurchaseOrderId",
+                table: "PurchaseOrderDetail",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PurchaseOrderDetailQuantity",
+                table: "PurchaseOrderDetail",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SupplierId",
+                table: "PurchaseOrder",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PurchaseOrderTotalPrice",
+                table: "PurchaseOrder",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PurchaseOrderDate",
+                table: "PurchaseOrder",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ProductWeight",
+                table: "Product",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ProductPrice",
+                table: "Product",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductName",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductDescription",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductColor",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BinName",
+                table: "Location",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "Inventory",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "Inventory",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inventory_Location_LocationId",
+                table: "Inventory",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inventory_Product_ProductId",
+                table: "Inventory",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchaseOrder_Supplier_SupplierId",
+                table: "PurchaseOrder",
+                column: "SupplierId",
+                principalTable: "Supplier",
+                principalColumn: "SupplierId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchaseOrderDetail_PurchaseOrder_PurchaseOrderId",
+                table: "PurchaseOrderDetail",
+                column: "PurchaseOrderId",
+                principalTable: "PurchaseOrder",
+                principalColumn: "PurchaseOrderId");
+        }
+    }
+}
