b61c51dc44cad645091af9eda18edb71582a91e8
Fue <fchang@students.ntc.edu>
2022-10-11 06:58:19 -0500

    Added Login view & model and account controller

---
+++ SRC/NTC_Lego/NTC_Lego/Server/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace NTC_Lego
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
