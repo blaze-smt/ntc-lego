cb65b137eade628d6fcdafd9e8edf04e6e08af74
Sarah Block <seblock@students.ntc.edu>
2022-10-12 14:00:01 -0500

    Get item image and bricklink url

--- SRC/NTC_Lego/NTC_Lego/Server/Services/DataService.cs
+++ SRC/NTC_Lego/NTC_Lego/Server/Services/DataService.cs
@@ -22,18 +22,7 @@ namespace NTC_Lego.Server.Services
 
         public Item GetItem(string ItemId)
         {
-            return _dataContext.Item.Include(x => x.ItemType).Include(x => x.Category).ToList().Find(x => x.ItemId == ItemId);
-        }
-
-        public List<Color> GetItemColors(string ItemId)
-        {
-            // API call to BrickLink
-            return null;
-        }
-        public string GetItemImage(string ItemId, string ColorId)
-        {
-            // API call to BrickLink, returns image path
-            return null;
+            return _dataContext.Item.ToList().Find(x => x.ItemId == ItemId);
         }
     }
 }
