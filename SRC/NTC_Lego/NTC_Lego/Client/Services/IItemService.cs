b2ef8030cb2eb21886d8d5aafd1b1367a48538de
Adam Domka <ajdomka@students.ntc.edu>
2022-10-12 20:08:13 -0500

    Added second reference project. Revised logic for populating Items table and added ItemView razor page with ability to view a single item.

---
+++ SRC/NTC_Lego/NTC_Lego/Client/Services/IItemService.cs
@@ -0,0 +1,9 @@
+﻿using NTC_Lego.Shared;
+
+namespace NTC_Lego.Client.Services
+{
+    public interface IItemService
+    {
+        Task<Item> GetItem(string itemId);
+    }
+}
--- SRC/NTC_Lego/NTC_Lego/Server/Controllers/ItemController.cs
+++ SRC/NTC_Lego/NTC_Lego/Server/Controllers/ItemController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using NTC_Lego.Server.Services;
 using NTC_Lego.Server.ViewModels;
@@ -9,66 +10,24 @@ using System.Security.Claims;
 
 namespace NTC_Lego.Server.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("[controller]")]
     public class ItemController : ControllerBase
     {
-        private readonly ILogger<ItemController> _logger;
-        //private readonly AppConfig _appConfig;
-        private readonly DataService _dataService;
+        private readonly DataContext _dataContext;
 
-        public ItemController(ILogger<ItemController> logger, DataContext dataContext)
+        public ItemController(DataContext dataContext)
         {
-            _logger = logger;
-            //_appConfig = appConfig.Value;
-            _dataService = new DataService(dataContext);
+            _dataContext = dataContext;
         }
 
-        [Route("Item/{id:int}")]
-        public IActionResult Item(int ItemId)
+        [HttpGet("{itemId}")]
+        public async Task<IActionResult> Get(string itemId)
         {
-            return null;
-/*            Item item = _dataService.GetItem(ItemId);
-            ItemViewModel ivm = new ItemViewModel();
-            ivm.ItemName = item.ItemName;
-            ivm.ItemId = item.ItemId;
-            ivm.ItemWeight = item.ItemWeight;
-            return Ok(ivm);
-*/        }
-
-        [AllowAnonymous]
-        [Route("Items")]
-        public IActionResult Items(string sort, string filter)
-        {
-            List<Item> items = _dataService.GetItems();
-            if (!String.IsNullOrEmpty(filter))
-            {
-                items = items
-                    .Where(i => i.ItemName.ToLower().Contains(filter.ToLower())
-                        || i.ItemTypeId.ToLower().Contains(filter.ToLower())
-                    )
-                    .ToList();
-            }
-/*            switch (sort)
-            {
-                case "new":
-                    items = items.Where(x => x.IsNew == true).ToList();
-                    break;
-                case "trending":
-                    items = items.Where(x => x.IsTrending == true).ToList();
-                    break;
-                case "popular":
-                    items = items.Where(x => x.IsPopular == true).ToList();
-                    break;
-                case "name_desc":
-                    items = items.OrderByDescending(x => x.Name).ToList();
-                    break;
-                default:
-                    items = items.OrderBy(x => x.Name).ToList();
-                    break;
-            }
-*/
-            return Ok(items);
+            var item = await _dataContext.Item.FirstOrDefaultAsync(x => x.ItemId == itemId);
+            if (item == null)
+                return NotFound("Item was not found.");
+            return Ok(item);
         }
     }
 }
